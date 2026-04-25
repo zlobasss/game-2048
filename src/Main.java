e458bd21ca2f8fac5864099dc8c320f8095c44ed
Torriandr <torri_alex_3705@vk.com>
2026-04-25 15:20:46 +0700

    init commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class HelloWorld2D extends JPanel {
+
+    private static double scale = 5;
+
+    public HelloWorld2D() {
+        Timer timer = new Timer(8, (ActionEvent e) -> {
+            update();
+            repaint();
+        });
+        timer.start();
+    }
+
+    private void update() {
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.BLUE);
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+
+        int size = 70;
+
+        int x = (int) ((panelWidth / scale - size) / 2);
+        int y = (int) ((panelHeight / scale - size) / 2);
+
+        g2d.scale(scale, scale);
+
+        g2d.drawRoundRect(x, y, size, size, 10, 10);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        HelloWorld2D panel = new HelloWorld2D();
+        panel.setPreferredSize(new Dimension(400, 400));
+        
+        frame.add(panel);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
